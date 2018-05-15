e48ffe79d9f9d93382993c6e428fd1a498d99bc0
rails -v <devashree.gosavi@gmail.com>
2018-05-15 23:35:47 +0530

    modified_on_15may_2335

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'admin',component:AdminComponent},
+  {path:'**',component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
